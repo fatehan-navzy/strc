b40b6a4f9065f9e13bbab2c2f84cac2890aa5e7a
Mehdi <96rajabi@gmail.com>
2025-08-08 12:27:11 +0330

    added device port static pool

--- strc.go
+++ strc.go
@@ -1,3 +1,7 @@
 package strc
 
 const DeviceAuthorize string = "device:authorize" // map[deviceId]proto(navzy.Device)
+
+func DevicePort() map[uint16]int32 {
+	return map[uint16]int32{10920: 1, 10921: 1}
+}
