93d4e3d5fa79a989543ad0346340821c010674f7
Mehdi <96rajabi@gmail.com>
2025-08-23 04:03:54 +0330

    added device dynamic to static key

--- strc.go
+++ strc.go
@@ -1,6 +1,7 @@
 package strc
 
-const DeviceAuthorize string = "device:authorize" // map[deviceId]proto(navzy.Device)
+const DeviceAuthorize string = "device:authorize"  // map[deviceId]proto(navzy.Device)
+const DynamicToStatic string = "dynamic:to:static" // map[string]string
 
 func DevicePort() map[uint16]int32 {
 	return map[uint16]int32{10920: 1, 10921: 1}
