09a9ec984189807ff0d080f94ddb7dfd3be8ceee
Mehdi <96rajabi@gmail.com>
2025-09-08 18:26:05 +0330

    updated protocols and services

--- ncu.sh
+++ ncu.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+set -e
 
 printf "Updating...\n"
 
 git pull
-go get -u all
+
+# Use modern idiomatic Go update
+go get -u ./...
+go mod tidy
+
+# Install protoc plugins at specific versions, ensure $GOPATH/bin in PATH
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
@@ -16,31 +22,38 @@ protoc \
   --validate_out="lang=go,paths=source_relative,runtime=true:." \
   -I ../strc \
   -I third_party \
-  ../strc/{devices,packets,services}/*.proto
+  ../strc/devices/*.proto \
+  ../strc/packets/*.proto \
+  ../strc/services/*.proto
 
-find . -type d -exec sh -c 'if ls "$1"/*.proto 1> /dev/null 2>&1; then mkdir -p "/home/mehdi/navzy/Navzy/app/src/main/proto/$(basename "$1")"; cp "$1"/*.proto "/home/mehdi/navzy/Navzy/app/src/main/proto/$(basename "$1")/"; fi' _ {} \;
+find ../strc -type d -exec bash -c \
+  'if compgen -G "$1/*.proto" > /dev/null; then \
+    mkdir -p "/home/mehdi/navzy/Navzy/app/src/main/proto/$(basename "$1")"; \
+    cp "$1"/*.proto "/home/mehdi/navzy/Navzy/app/src/main/proto/$(basename "$1")/"; \
+  fi' _ {} \;
 rm -rf /home/mehdi/navzy/Navzy/app/src/main/proto/validate
 
 printf "Compiling Completed\n"
 
-printf "Commiting and Pushing\n"
+printf "Committing and Pushing\n"
 
 git add .
-git commit -m "updated protocols and services"
-git push orging main
+if ! git diff --cached --quiet; then
+  git commit -m "updated protocols and services"
+  git push
+else
+  printf "No changes to commit.\n"
+fi
 
-printf "Commited and Pushed\n"
+printf "Committed and Pushed\n"
 
-printf "Updating Services"
+printf "Updating Services\n"
 
-cd ../gnet || exit
-GOPRIVATE=github.com/fatehan-navzy go get -u github.com/fatehan-navzy/strc@latest
-printf "GNET updated\n"
-
-cd ../gowo || exit
-GOPRIVATE=github.com/fatehan-navzy go get -u github.com/fatehan-navzy/strc@latest
-printf "GOWO updated\n"
-
-cd ../grpc || exit
-GOPRIVATE=github.com/fatehan-navzy go get -u github.com/fatehan-navzy/strc@latest
-printf "GRPC updated\n"
+for dir in ../gnet ../gowo ../grpc; do
+  if [ -d "$dir" ]; then
+    cd "$dir"
+    GOPRIVATE=github.com/fatehan-navzy go get -u github.com/fatehan-navzy/strc@latest
+    printf "$(basename "$dir") updated\n"
+    cd - > /dev/null
+  fi
+done
